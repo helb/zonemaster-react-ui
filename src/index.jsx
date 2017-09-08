a72f3845b7f8ab03276f0e9e806770ccc50ded17
helb <helb@helb.cz>
2017-09-08 11:20:25 +0200

    Enable HMR.

--- src/index.jsx
+++ src/index.jsx
@@ -5,3 +5,11 @@ import App from './App.jsx';
 const mountNode = document.createElement('div');
 document.body.appendChild(mountNode);
 ReactDOM.render(<App />, mountNode);
+
+const render = () => {
+  ReactDOM.render(<App />, mountNode);
+};
+
+render();
+
+module.hot.accept('./App.jsx', render);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
   entry: {
-    app: ['babel-polyfill', './src/index.jsx']
+    app: ['babel-polyfill', 'react-hot-loader/patch', './src/index.jsx']
   },
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -14,8 +15,13 @@ module.exports = {
   module: {
     rules: [{ test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }]
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin()
+  ],
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
+    port: 3000
   }
 };
