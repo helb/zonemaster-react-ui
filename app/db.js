c2538bff75ec6793a1acd0ec12d784601170b75c
Jiří Helebrant <jiri.helebrant@nic.cz>
2017-11-22 11:19:02 +0100

    Remove unnecessary awaits, correct result list sorting

--- app/db.js
+++ app/db.js
@@ -7,15 +7,15 @@ db.version(1).stores({
 });
 
 export default {
-  add: async result => await db.table('results').add(result),
+  add: async result => db.table('results').add(result),
 
-  count: async () => await db.table('results').count(),
+  count: async () => db.table('results').count(),
 
   list: async (key = 'date', order = 'desc') => {
     const results = await db
       .table('results')
       .toCollection()
       .sortBy(key);
-    return order === 'desc' ? results : results.reverse();
+    return order === 'desc' ? results.reverse() : results;
   }
 };
