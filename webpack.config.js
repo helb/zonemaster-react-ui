5c5eb6305500f158d87627c61976bf7d3cf54a6f
Jiří Helebrant <jiri.helebrant@nic.cz>
2017-11-29 01:46:29 +0100

    Remove favicons-webpack-plugin from webpack dev config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   devtool: 'eval-source-map',
