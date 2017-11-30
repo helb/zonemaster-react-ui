9a081dcbe3c4281f0e024b860228ed69d10569fc
Jiří Helebrant <jiri.helebrant@nic.cz>
2017-11-30 13:26:47 +0100

    Use Router instead of HashRouter

--- app/App.jsx
+++ app/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 import TestResult from './TestResult';
 import TestForm from './TestForm';
