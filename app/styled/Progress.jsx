b4ca5281e80a676c6dafaa7c5cb2e91c5d315c49
Jiří Helebrant <jiri.helebrant@nic.cz>
2017-12-04 17:02:51 +0100

    Disable the stylelint rules broken by update

--- app/styled/Progress.jsx
+++ app/styled/Progress.jsx
@@ -7,8 +7,10 @@ export default styled.div`
   color: ${props => (props.value < 90 ? '#666' : 'white')};
   font-size: 1rem;
   line-height: 1.4em;
-  padding-left: ${props =>
-    (props.value < 90 ? `calc(${props.value}% + 0.25em)` : `calc(${props.value}% - 3em)`)};
+  padding-left: ${props => // displays the value inside the filled part for >90%
+    (props.value < 90
+      ? `calc(${props.value}% + 0.25em)`
+      : `calc(${props.value}% - 3em)`)};
   background: ${props =>
     (props.value
       ? `linear-gradient(to right, #666 ${props.value}%, white calc(${props.value}% + 1px));`
