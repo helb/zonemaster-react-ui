4f856e8e2b9f1a8d765e5f2bc850c2e2835d3c10
Jiří Helebrant <jiri.helebrant@nic.cz>
2018-03-12 12:49:06 +0100

    Update Webpack config to v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,40 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval-source-map',
-  entry: [
-    'babel-polyfill',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    'react-hot-loader/patch',
-    path.join(__dirname, 'app/index.jsx')
-  ],
+  mode: 'development',
+  entry: ['babel-polyfill', path.join(__dirname, 'app/index.jsx')],
   output: {
-    path: path.join(__dirname, '/dist/'),
-    filename: '[name].js',
-    publicPath: '/'
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].[hash:8].js',
+    chunkFilename: '[name].[chunkhash:8].js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.svg']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.svg$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'svg-react-loader'
+        }
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.tpl.html',
       inject: 'body',
       filename: 'index.html'
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
     })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-react-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    port: 3000,
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-    quiet: false,
-    noInfo: false,
-    stats: {
-      assets: false,
-      colors: true,
-      version: false,
-      hash: false,
-      timings: false,
-      chunks: false,
-      chunkModules: false
-    }
-  }
+  ]
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,54 +1,20 @@
-const path = require('path');
+const devConfig = require('./webpack.config.js');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const webpack = require('webpack');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const merge = require('webpack-merge');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const WebappWebpackPlugin = require('webapp-webpack-plugin');
 
-module.exports = {
+module.exports = merge(devConfig, {
+  mode: 'production',
   devtool: 'nosources-source-map',
-  // The entry file. All your app roots from here.
-  entry: [
-    // Polyfills go here too, like babel-polyfill or whatwg-fetch
-    'babel-polyfill',
-    path.join(__dirname, 'app/index.jsx')
-  ],
-  // Where you want the output to go
-  output: {
-    path: path.join(__dirname, '/public/'),
-    filename: '[name].[chunkhash].js',
-    publicPath: '/'
-  },
   plugins: [
-    // webpack gives your modules and chunks ids to identify them. Webpack can vary the
-    // distribution of the ids to get the smallest id length for often used ids with
-    // this plugin
-    new webpack.optimize.OccurrenceOrderPlugin(),
-
-    // handles creating an index.html file and injecting assets. necessary because assets
-    // change name because the hash part changes. We want hash name changes to bust cache
-    // on client browsers.
-    new HtmlWebpackPlugin({
-      title: 'Zonemaster',
-      template: 'app/index.tpl.html',
-      inject: 'body',
-      filename: 'index.html',
-      minify: {
-        collapseWhitespace: true,
-        minifyCSS: true,
-        removeAttributeQuotes: true
-      }
-    }),
-    new FaviconsWebpackPlugin({
+    new WebappWebpackPlugin({
       logo: './app/assets/icons/favicon.svg',
-      prefix: 'icons-[hash:10]/',
+      prefix: 'icons-[hash:8]/',
       title: 'Zonemaster',
       icons: {
         android: true,
@@ -63,70 +29,24 @@ module.exports = {
         windows: false
       }
     }),
-    new CopyWebpackPlugin([{ from: './app/assets/og-image.png' }]),
-    // extracts the css from the js files and puts them on a separate .css file. this is for
-    // performance and is used in prod environments. Styles load faster on their own .css
-    // file as they dont have to wait for the JS to load.
-    new ExtractTextPlugin('[name]-[hash].min.css'),
-    // handles uglifying js
-    new UglifyJsPlugin({
-      sourceMap: true
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: m => /node_modules/.test(m.context)
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'react',
-      minChunks: m => /node_modules\/(?:react|react-router|babel|dexie|styl)/.test(m.context)
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'router',
-      minChunks: m => /node_modules\/(?:react-router|babel|dexie|styl)/.test(m.context)
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'dexie',
-      minChunks: m => /node_modules\/(?:babel|dexie|styl)/.test(m.context)
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'styled',
-      minChunks: m => /node_modules\/(?:babel|styl)/.test(m.context)
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'polyfill',
-      minChunks: m => /node_modules\/(?:babel)/.test(m.context)
-    }),
-    // plugin for passing in data to the js, like what NODE_ENV we are in.
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    new CopyWebpackPlugin([{ from: './app/assets/og-image.png' }])
   ],
-  module: {
-    // loaders handle the assets, like transforming sass to css or jsx to js.
-    rules: [
-      {
-        enforce: 'pre', // Runs before other loaders
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-react-loader'
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          enforce: true
+        }
       }
+    },
+    minimizer: [
+      new UglifyJsPlugin({
+        parallel: true,
+        sourceMap: true
+      })
     ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
   }
-  // postcss: [require('autoprefixer')]
-};
+});
